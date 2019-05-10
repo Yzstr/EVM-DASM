525525d9f1ac9b74e89ece9cca4a57ae9949f9a3
Yzstr An <yzstr@outlook.com>
2019-05-10 13:47:13 +0800

    get rid of shits

--- EVM-DASM.py
+++ EVM-DASM.py
@@ -1,80 +1,140 @@
 from MAP import *
 import sys
+import json
+import sha3
+from pprint import pprint
 
 
+def split_byteCode(byteCode_file, isDeploy=True):
+    with open(byteCode_file, 'r') as f:
+        byteCode = f.read()
+    contract_start = 0
+    contract_end = 0       # How to find the end if the end is not the begin of bzzr?
+    bzzr_start = 0
+    augs_start = 0
+    deployment = ''
+    contract = ''
+    bzzr = ''
+    augs = []
 
-def split_byteCode(byteCode,isDeploy=True):
-    contract_start=0
-    contract_end=0       # How to find the end if the end is not the begin of bzzr?
-    bzzr_start=0
-    augs_start=0
-    deployment=''
-    contract=''
-    bzzr=''
-    augs=[]
-
-    byteCode=byteCode.lower()
+    byteCode = byteCode.lower()
     if isDeploy:
-        contract_start=byteCode.find('f300')
-        contract_start+=4
+        contract_start = byteCode.find('f300')
+        contract_start += 4
 
-    bzzr_start=byteCode.find('a165627a7a72305820')
-    bzzr='bzzr: '+byteCode[bzzr_start+18:bzzr_start+18+64]
-    augs_start=bzzr_start+18+64+4
+    bzzr_start = byteCode.find('a165627a7a72305820')
+    bzzr = 'bzzr: '+byteCode[bzzr_start+18:bzzr_start+18+64]
+    augs_start = bzzr_start+18+64+4
 
-    for i in range(augs_start,len(byteCode),64):
-        aug=str('Arg [{}]: '.format(str((i-augs_start) // 64))+byteCode[i:i+64])
+    for i in range(augs_start, len(byteCode), 64):
+        aug = str('Arg [{}]: '.format(
+            str((i-augs_start) // 64))+byteCode[i:i+64])
         augs.append(aug)
-    deployment=byteCode[:contract_start]
-    contract=byteCode[contract_start:bzzr_start]
-    return deployment,contract,bzzr,augs
-    
+    deployment = byteCode[:contract_start]
+    contract = byteCode[contract_start:bzzr_start]
+    return deployment, contract, bzzr, augs
+
 
 def get_opCode_list(byteCode):
-    opCode_list=[]
-    push_skip_times=0
-    for i in range(0,len(byteCode),2):
-        code=byteCode[i:i+2]
-        if push_skip_times==0:
-            loc='0x'+str(hex(i//2))[2:].zfill(8)
+    opCode_list = []
+    push_skip_times = 0
+    for i in range(0, len(byteCode), 2):
+        code = byteCode[i:i+2]
+        if push_skip_times == 0:
+            loc = '0x'+str(hex(i//2))[2:].zfill(8)
             if code[0] != '6' and code[0] != '7':
-                opCode=[loc,code,opCodes.get(code,'<------- ERROR'),'']
+                opCode = [loc, code, opCodes.get(code, '<------- ERROR'), '']
                 opCode_list.append(opCode)
             else:
-                push_skip_times=push_map[code]
-                opCode=[loc,code,opCodes.get(code,'<------- ERROR'),byteCode[i+2:i+2+push_skip_times*2]]
+                push_skip_times = push_map[code]
+                opCode = [loc, code, opCodes.get(
+                    code, '<------- ERROR'), byteCode[i+2:i+2+push_skip_times*2]]
                 opCode_list.append(opCode)
         else:
-            push_skip_times-=1
+            push_skip_times -= 1
             continue
     return opCode_list
 
 
-def print_opCode(opCode_list):
-    for opCode in opCode_list:
-        print(opCode[0]+':  '+opCode[1]+'   '+opCode[2]+'       '+opCode[3])
-    print('\n\n\n')
+def print_opCode(opCode_list, abi_hash_dict={}, isContract=False):
+    if not isContract:
+        for opCode in opCode_list:
+            print(opCode[0]+':  '+opCode[1]+'   ' +
+                  opCode[2]+'       '+opCode[3])
+        print('\n\n\n')
+    else:
+        for i in range(len(opCode_list)):
+            if opCode_list[i][1] == '80':
+                if opCode_list[i+1][1] == '63' and opCode_list[i+2][1] == '14' and opCode_list[i+3][1] == '61' and opCode_list[i+4][1] == '57':
+                    function_name_hash = opCode_list[i+1][3]
+                    function_name = abi_hash_dict[function_name_hash][0]
+                    abi_hash_dict[function_name_hash][1] = opCode_list[i+3][3]
+                    print('\nFunction Head:   '+function_name)
+            function_check = get_function_name_by_address(
+                opCode_list[i][0][-4:], abi_hash_dict)
+            if function_check != None:
+                print('\nFunction Body:   '+function_check)
+            print(opCode_list[i][0]+':  '+opCode_list[i][1] +
+                  '   '+opCode_list[i][2]+'       '+opCode_list[i][3])
+            if opCode_list[i][1] == '00' or opCode_list[i][1] == 'f3':
+                print('\n')
+        print('\n\n\n')
 
 
+def get_function_name_by_address(address, abi_hash_dict):
+    for abi_info in abi_hash_dict.values():
+        if address == abi_info[1]:
+            return abi_info[0]
+    return None
 
 
+def get_abi_hash_dict(abi_file):
+    with open(abi_file, 'r') as f:
+        abi_data_all = json.load(f)
 
+    function_name_list = []
 
+    for abi_data in abi_data_all:
+        if abi_data['type'] == 'function':
+            name = abi_data['name']
+            name += '('
+            if not abi_data['inputs']:
+                name += ')'
+            else:
+                for input_aug in abi_data['inputs']:
+                    name += input_aug['type']
+                    name += ','
+                name = name[:-1]
+                name += ')'
+            function_name_list.append(name)
+    abi_hash_dict = {}
+    for name in function_name_list:
+        hashed_string = sha3.keccak_256(
+            name.encode('utf-8')).hexdigest().lower()[:8]
+        abi_hash_dict[hashed_string] = [name, '']
+    return abi_hash_dict
 
 
 if __name__ == "__main__":
-    binary_file=sys.argv[1]
-    with open(binary_file,'r') as f:
-        binary=f.read()
-    
-    deployment,contract,bzzr,augs=split_byteCode(binary)
-    deploy_list=get_opCode_list(deployment)
-    contract_list=get_opCode_list(contract)
+    binary_file = sys.argv[1]
+    abi_file = sys.argv[2]
+
+    abi_hash_dict = get_abi_hash_dict(abi_file)
+
+    pprint(abi_hash_dict)
+    print('\n\n')
+
+    deployment, contract, bzzr, augs = split_byteCode(binary_file)
+
+    deploy_list = get_opCode_list(deployment)
+    contract_list = get_opCode_list(contract)
 
     print_opCode(deploy_list)
-    print_opCode(contract_list)
+    print_opCode(contract_list, abi_hash_dict, True)
 
-    print(bzzr+"\n\n")
-    print(augs)
-
-
+    
+    pprint(abi_hash_dict)
+    print('\n\n')
+    pprint(bzzr)
+    print('\n\n')
+    pprint(augs)
--- MAP.py
+++ MAP.py
@@ -224,3 +224,108 @@ def print_gap(choice):
         print('ABI Augs')
         print('***********************************', end='')
         print('***********************************')
+
+
+def get_opcode(string):
+    skip_times = 0
+    first_stop = 0
+    log_out = False
+    last_stop = 0
+
+    print_gap(0)
+    for _ in range(2):
+        for i in range(0, len(string), 2):
+            code = string[i:i+2]
+            if skip_times != 0:
+                if log_out:
+                    print(code, end='')
+                skip_times -= 1
+                if skip_times == 0:
+                    if log_out:
+                        print('')
+                continue
+
+            if code == '00':
+                if first_stop == 0:
+                    first_stop = i
+                if not log_out:
+                    last_stop = i
+                else:
+                    print(code+": "+opCodes[code])
+                    if i == first_stop:
+                        print_gap(1)
+                    if i == last_stop:
+                        break
+                    continue
+
+            if code[0] != '6' and code[0] != '7':
+                if code in opCodes.keys():
+                    if log_out:
+                        print(code+": "+opCodes[code])
+                else:
+                    if log_out:
+                        print(code+': <------------ ERROR')
+                        print('The Contract Opcode ends at {}.\\'.format(last_stop))
+                    break
+
+            else:
+                n = push_map[code]
+
+                if log_out:
+                    print(code+": PUSH"+str(n)+" ", end='')
+                skip_times = n
+
+        log_out = True
+        # print(last_stop)
+
+    data = string[last_stop:]
+    bzzr_begin = data.find('a165627a7a72305820')
+    if log_out:
+        print_gap(2)
+        print(data[:bzzr_begin], end='\n\n')
+
+    if bzzr_begin > -1:
+        if log_out:
+            print_gap(3)
+            print("bzzr: "+data[bzzr_begin+18:bzzr_begin+18+64])
+        abi_begin = bzzr_begin+18+64+4
+        print_gap(4)
+        for i in range(abi_begin, len(data), 64):
+            if log_out:
+                print('Arg [{}]: '.format(str(i // 64))+data[i:i+64])
+    else:
+        if log_out:
+            print('bzzr not found')
+
+import json
+from pprint import pprint
+
+def decode_abi():
+    function_name=[]
+
+    with open('bnb.abi','r') as f:
+        data=json.load(f)
+    # print(data)
+    for dict in data:
+        if dict["type"]=="function":
+            # print(dict["name"])
+            name=dict["name"]
+            name+='('
+            if not dict['inputs']:
+                name+=')'
+            else:
+                for sub_dict in dict['inputs']:
+                    name+=sub_dict['type']
+                    name+=','  
+                name=name[:-1]
+                name+=')'          
+            
+            function_name.append(name)
+
+    pprint(function_name)
+
+    function_name_hash=dict.fromkeys(function_name)
+    for name in function_name_hash.keys():
+        function_name_hash[name]=sha3.keccak_256(name.encode('utf-8')).hexdigest().upper()[:8]
+
+    pprint(function_name_hash)
